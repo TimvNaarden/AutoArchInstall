a5fddf58590eb1ea1dd2ad0bc3d6eb332fbb8c50
Tim van Naarden <TimvNaarden@outlook.com>
2025-06-28 00:23:45 +0200

    Update

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+sudo pacman -Syu
+
 #Install dependencies
 sudo pacman -S git stow btop dunst hyprland kitty waybar wofi zsh curl udiskie grim slurp wl-clipboard thunar zoxide tmux inotify-tools neovim firefox NetworkManager base-devel
 
@@ -26,12 +28,13 @@ cd ..
 #Sddm theme 
 sudo touch /etc/sddm.conf
 sudo echo "[Theme] \nCurrent=catppuccin-mocha" > /etc/sddm.conf
-sudo pacman -S qt6-svg qt6-declarative qt5-quickcontrols2 unzip
-curl -O https://github.com/catppuccin/sddm/releases/download/v1.1.0/catppuccin-mocha.zip
-sudo unzip catppuccin-mocha.zip -d /usr/share/sddm/themes
+sudo pacman -S qt6-svg qt6-declarative qt5-quickcontrols2 unzip wget
+wget https://github.com/catppuccin/sddm/releases/download/v1.1.0/catppuccin-mocha.zip
+sudo unzip catppuccin-mocha.zip -d /usr/share/sddm/themes/
+sudo rm catppuccin-mocha.zip
 
 #Applications
-sudo pacman -S blueman pavucontrol
+sudo pacman -S blueman pavucontrol networkmanager sddm udiskie
 sudo systemctl enable NetworkManager
 sudo systemctl enable udisks2
 sudo systemctl enable sddm
