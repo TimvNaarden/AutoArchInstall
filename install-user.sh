ed209271ac21b0ff9e119dcadd2fa872ab8e0319
Tim van Naarden <TimvNaarden@outlook.com>
2025-06-28 00:40:10 +0200

    Update

---
+++ install-user.sh
@@ -0,0 +1,20 @@
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+cd .. && rm -rf yay
+
+#More dependencies 
+yay -S wlogout swaylock-effects swww
+
+#Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+chsh -s $(which zsh)
+
+#Tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+#Dotfiles
+git clone https://github.com/timvnaarden/dotfiles ~/dotfiles
+cd ~/dotfiles
+stow .
+cd ..
